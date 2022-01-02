db709957052b295ed7b15cc2da58c71d14c2374e
Dmitriy <fadeev97-1997@yandex.ru>
2022-01-02 20:08:31 +0400

    created blur handler and warning element

--- src/components/chips-input/chips-input.js
+++ src/components/chips-input/chips-input.js
@@ -5,6 +5,7 @@ import ChipsItem from "../chips-item";
 export default function ChipsInput({ value, onChange }) {
   const [input, setInput] = useState("");
   const [chips, setChips] = useState([]);
+  const [warning, setWarning] = useState(false);
   const spanEl = useRef(5);
   const inputEl = useRef(null);
 
@@ -12,26 +13,48 @@ export default function ChipsInput({ value, onChange }) {
     setInput(event.target.value);
   };
 
+  const blurHandler = () => {
+    if (input.length !== 0) {
+      if (input.split('"').length % 2 === 1) {
+        setWarning(false);
+        setChips([...chips, input]);
+        setInput("");
+      } else {
+        setWarning(true);
+      }
+    }
+  };
+
   const inputFocus = () => {
     inputEl.current?.focus();
   };
 
   const addChips = (event) => {
-    if (event.key === ",") {
+    if (event.key === "," && input.split('"').length % 2 === 1) {
+      if (input.length === 0) {
+        event.preventDefault();
+        return;
+      }
       event.preventDefault();
       setChips([...chips, event.target.value]);
       setInput("");
     }
   };
+
+  const placeholder =
+    input.length === 0 && chips.length === 0 ? (
+      <div className={styles.placeholder}>Введите ключевые слова</div>
+    ) : null;
+
+  const warningEl = warning ? (
+    <div className={styles.warning}>Закройте кавычки с двух сторон</div>
+  ) : null;
+
   return (
     <div className={styles.chips_input}>
       <div className={styles.wrapper} onClick={inputFocus}>
         {chips.map((chip, index) => (
-          <ChipsItem 
-          key={index} 
-          chip={chip} 
-          input={input}
-          />
+          <ChipsItem key={index} chip={chip} input={input} />
         ))}
         <div className={styles.container}>
           <span className={styles.hide_el} ref={spanEl}>
@@ -42,12 +65,19 @@ export default function ChipsInput({ value, onChange }) {
             className={styles.input}
             value={input}
             onChange={changeHandler}
-            style={ input.length !== 0 ? { width: spanEl.current.offsetWidth + 14 + "px" } : { width: "2px" }}
+            onBlur={blurHandler}
+            style={
+              input.length !== 0
+                ? { width: spanEl.current.offsetWidth + 14 + "px" }
+                : { width: "2px" }
+            }
             ref={inputEl}
             onKeyDown={addChips}
           />
         </div>
+        {placeholder}
       </div>
+      {warningEl}
     </div>
   );
 }
